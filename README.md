642309c5b818930ef96124d09cc211172f0a8b53
myojin223 <64243555+myojin223@users.noreply.github.com>
2022-05-25 21:40:24 -0600

    Initial upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # WillieTanner
  Proximity template for 0.6.2+ in the style of Willie Tanner
+
+## Drag and drop the templates folder into your main Proximity directory
---
+++ Willie Tanner.bat
@@ -0,0 +1,2 @@
+java -jar proximity.jar --cards=Decklist.txt --art_source=BEST --reminder_text=true --template=WillieTanner --use_card_back=false --watermark=NONE
+cmd /k
